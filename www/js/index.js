8b9357293f4016584b9e307e100ca1ee2e3bd7ad
Jordan Coulson <jcoulson31@gmail.com>
2013-03-04 11:32:23 +0000

    logging zzzzzzz

--- www/js/index.js
+++ www/js/index.js
@@ -30,9 +30,9 @@ var app = {
         console.log("Device is ready!")
     },
     report: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        //var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
 
         //listeningElement.setAttribute('style', 'display:none;');
         //receivedElement.setAttribute('style', 'display:block;');
